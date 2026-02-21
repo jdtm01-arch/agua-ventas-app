34d113d33d125bf8a6d4c724108f06e6bc049160
Jose T <jdtm01@gmail.com>
2026-02-20 19:36:31 -0500

    PAGINACION LIMITE 25

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const NOMBRE_EMPRESA = 'Wankagua'
 export const SLOGAN = 'Incontrastablemente Pura'
 export const LIMITE_EDICION = 60 //tambien configurar config/limits.php
-export const PAGINACION = 1
+export const PAGINACION = 25
